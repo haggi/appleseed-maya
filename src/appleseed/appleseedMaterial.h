a82007500d4c125e3bc50eee6e70173691a3aaaa
haggi krey <haggi@haggi.de>
2016-01-22 20:28:47 +0100

    remove all mixed case files

--- src/appleseed/appleseedMaterial.h
+++
@@ -1,9 +0,0 @@
-#ifndef APPLESEED_MATERIAL_H
-#define APPLESEED_MATERIAL_H
-
-#include "renderer/api/material.h"
-
-namespace AppleRender{
-	void updateMaterial(MObject materialNode, const asr::Assembly *assembly);
-}
-#endif
