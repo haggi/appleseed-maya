37a4417c42b7f639aa0a5a58e83e548f31164967
haggi krey <haggi@haggi.de>
2016-01-23 14:44:55 +0100

    lower case files

---
+++ mtap_devmodule/scripts/plug-ins/loadshadersplugin.py
@@ -0,0 +1,15 @@
+import math, sys
+
+import maya.OpenMaya as OpenMaya
+import maya.OpenMayaMPx as OpenMayaMPx
+import appleseed.metashadernode as meta
+import appleseed.shadingnodes as shadingNodes
+        
+def initializePlugin(mobject):
+    mplugin = OpenMayaMPx.MFnPlugin(mobject)    
+    shadingNodes.registerShadingNodes(mplugin)
+
+def uninitializePlugin(mobject):
+    mplugin = OpenMayaMPx.MFnPlugin(mobject)
+    shadingNodes.unregisterShadingNodes(mplugin)
+    
