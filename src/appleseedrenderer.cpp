4e228cf0f44868bcfe928b1f6d2999406f7c21cc
haggi krey <haggi@haggi.de>
2016-03-13 18:53:39 +0100

    Sunlight update.

--- src/appleseedrenderer.cpp
+++ src/appleseedrenderer.cpp
@@ -492,135 +492,134 @@ void AppleseedRenderer::defineEnvironment()
 
     switch (environmentType)
     {
-    case 0: //constant
-    {
-        environmentEDF = renderer::ConstantEnvironmentEDFFactory().create(
-                "sky_edf",
-                renderer::ParamArray()
-                .insert("radiance", envColorName));
-        break;
-    }
-    case 1: //ConstantHemisphere
-    {
-        environmentEDF = renderer::ConstantHemisphereEnvironmentEDFFactory().create(
-            "sky_edf",
-            renderer::ParamArray()
-            .insert("radiance", envColorName)
-            .insert("upper_hemi_radiance", gradHorizName)
-            .insert("lower_hemi_radiance", gradHorizName));
-        break;
-    }
-    case 2: //Gradient
-    {
-        environmentEDF = renderer::GradientEnvironmentEDFFactory().create(
-                "sky_edf",
-                renderer::ParamArray()
-                .insert("horizon_radiance", gradHorizName)
-                .insert("zenith_radiance", gradZenitName)
-                );
-        break;
-    }
-    case 3: //Latitude Longitude
-    {
-        environmentEDF = renderer::LatLongMapEnvironmentEDFFactory().create(
-                "sky_edf",
-                renderer::ParamArray()
-                .insert("radiance", envMapAttrName.asChar())
-                .insert("radiance_multiplier", environmentIntensity)
-                .insert("horizontal_shift", latlongHoShift)
-                .insert("vertical_shift", latlongVeShift)
-                );
-        break;
-    }
-    case 4: //Mirror Ball
-    {
-        environmentEDF = renderer::MirrorBallMapEnvironmentEDFFactory().create(
-                "sky_edf",
-                renderer::ParamArray()
-                .insert("radiance", envMapAttrName.asChar())
-                .insert("radiance_multiplier", environmentIntensity));
-        break;
-    }
-    case 5: //Physical Sky
-    {
-        foundation::Vector3d unitVector(0.0, 0.0, 0.0);
-        float sun_theta = getFloatAttr("sun_theta", appleseedGlobals, 1.0f);
-        float sun_phi = getFloatAttr("sun_phi", appleseedGlobals, 1.0f);
-        bool usePhysicalSun = getBoolAttr("physicalSun", appleseedGlobals, true);
-        double theta = 90.0f - sun_theta, phi = sun_phi;
-
-        if (usePhysicalSun)
+        case 0: //constant
         {
-            // get the connected sun light
-            // physicalSunConnection
-            MObject connectedNode = getConnectedInNode(getRenderGlobalsNode(), "physicalSunConnection");
-            if (connectedNode != MObject::kNullObj)
+            environmentEDF = renderer::ConstantEnvironmentEDFFactory().create(
+                    "sky_edf",
+                    renderer::ParamArray()
+                    .insert("radiance", envColorName));
+            break;
+        }
+        case 1: //ConstantHemisphere
+        {
+            environmentEDF = renderer::ConstantHemisphereEnvironmentEDFFactory().create(
+                "sky_edf",
+                renderer::ParamArray()
+                .insert("radiance", envColorName)
+                .insert("upper_hemi_radiance", gradHorizName)
+                .insert("lower_hemi_radiance", gradHorizName));
+            break;
+        }
+        case 2: //Gradient
+        {
+            environmentEDF = renderer::GradientEnvironmentEDFFactory().create(
+                    "sky_edf",
+                    renderer::ParamArray()
+                    .insert("horizon_radiance", gradHorizName)
+                    .insert("zenith_radiance", gradZenitName)
+                    );
+            break;
+        }
+        case 3: //Latitude Longitude
+        {
+            environmentEDF = renderer::LatLongMapEnvironmentEDFFactory().create(
+                    "sky_edf",
+                    renderer::ParamArray()
+                    .insert("radiance", envMapAttrName.asChar())
+                    .insert("radiance_multiplier", environmentIntensity)
+                    .insert("horizontal_shift", latlongHoShift)
+                    .insert("vertical_shift", latlongVeShift)
+                    );
+            break;
+        }
+        case 4: //Mirror Ball
+        {
+            environmentEDF = renderer::MirrorBallMapEnvironmentEDFFactory().create(
+                    "sky_edf",
+                    renderer::ParamArray()
+                    .insert("radiance", envMapAttrName.asChar())
+                    .insert("radiance_multiplier", environmentIntensity));
+            break;
+        }
+        case 5: //Physical Sky
+        {
+            foundation::Vector3d unitVector(0.0, 0.0, 0.0);
+            float sun_theta = getFloatAttr("sun_theta", appleseedGlobals, 1.0f);
+            float sun_phi = getFloatAttr("sun_phi", appleseedGlobals, 1.0f);
+            bool usePhysicalSun = getBoolAttr("physicalSun", appleseedGlobals, true);
+            double theta = 90.0f - sun_theta, phi = sun_phi;
+
+            if (usePhysicalSun)
             {
-                MFnTransform tn(connectedNode);
-                MMatrix tm = tn.transformationMatrix(&stat);
-                if (stat)
+                // get the connected sun light
+                // physicalSunConnection
+                MObject connectedNode = getConnectedInNode(getRenderGlobalsNode(), "physicalSunConnection");
+                if (connectedNode != MObject::kNullObj)
                 {
-                    MVector sunOrient(0,0,1);
-                    sunOrient *= tm;
-                    sunOrient.normalize();
-                    unitVector.x = sunOrient.x;
-                    unitVector.y = sunOrient.y;
-                    unitVector.z = sunOrient.z;
-                    renderer::unit_vector_to_angles(unitVector, theta, phi);
-                    theta = foundation::rad_to_deg(theta);
-                    sun_theta = foundation::rad_to_deg(theta);
-                    sun_phi = foundation::rad_to_deg(phi);
+                    MFnTransform tn(connectedNode);
+                    MMatrix tm = tn.transformationMatrix(&stat);
+                    if (stat)
+                    {
+                        MVector sunOrient(0,0,1);
+                        sunOrient *= tm;
+                        sunOrient.normalize();
+                        unitVector.x = sunOrient.x;
+                        unitVector.y = sunOrient.y;
+                        unitVector.z = sunOrient.z;
+                        renderer::unit_vector_to_angles(unitVector, theta, phi);
+                        sun_theta = foundation::rad_to_deg(theta);
+                        sun_phi = foundation::rad_to_deg(phi);
+                    }
+                }
+                else
+                {
+                    Logging::warning("physicalSunConnection plug has no connection, but use physical sun is turned on. Please correct.");
                 }
             }
-            else
+
+            if (skyModel == 0) // preetham
             {
-                Logging::warning("physicalSunConnection plug has no connection, but use physical sun is turned on. Please correct.");
+                environmentEDF = renderer::PreethamEnvironmentEDFFactory().create(
+                        "sky_edf",
+                        renderer::ParamArray()
+                        .insert("horizon_shift", horizon_shift)
+                        .insert("luminance_multiplier", luminance_multiplier)
+                        .insert("saturation_multiplier", saturation_multiplier)
+                        .insert("sun_phi", sun_phi)
+                        .insert("sun_theta", sun_theta)
+                        .insert("turbidity", turbidity)
+                        .insert("turbidity_max", turbidity_max)
+                        .insert("turbidity_min", turbidity_min));
             }
-        }
+            else
+            { // hosek
+                environmentEDF = renderer::HosekEnvironmentEDFFactory().create(
+                        "sky_edf",
+                        renderer::ParamArray()
+                        .insert("ground_albedo", ground_albedo)
+                        .insert("horizon_shift", horizon_shift)
+                        .insert("luminance_multiplier", luminance_multiplier)
+                        .insert("saturation_multiplier", saturation_multiplier)
+                        .insert("sun_phi", sun_phi)
+                        .insert("sun_theta", 90.0f - sun_theta)
+                        .insert("turbidity", turbidity)
+                        .insert("turbidity_max", turbidity_max)
+                        .insert("turbidity_min", turbidity_min));
+            }
 
-        if (skyModel == 0) // preetham
+            break;
+        }
+        case 6: //OSL Sky
         {
-            environmentEDF = renderer::PreethamEnvironmentEDFFactory().create(
-                    "sky_edf",
-                    renderer::ParamArray()
-                    .insert("horizon_shift", horizon_shift)
-                    .insert("luminance_multiplier", luminance_multiplier)
-                    .insert("saturation_multiplier", saturation_multiplier)
-                    .insert("sun_phi", sun_phi)
-                    .insert("sun_theta", sun_theta)
-                    .insert("turbidity", turbidity)
-                    .insert("turbidity_max", turbidity_max)
-                    .insert("turbidity_min", turbidity_min));
-        }
-        else
-        { // hosek
-            environmentEDF = renderer::HosekEnvironmentEDFFactory().create(
-                    "sky_edf",
-                    renderer::ParamArray()
-                    .insert("ground_albedo", ground_albedo)
-                    .insert("horizon_shift", horizon_shift)
-                    .insert("luminance_multiplier", luminance_multiplier)
-                    .insert("saturation_multiplier", saturation_multiplier)
-                    .insert("sun_phi", sun_phi)
-                    .insert("sun_theta", 90.0f - sun_theta)
-                    .insert("turbidity", turbidity)
-                    .insert("turbidity_max", turbidity_max)
-                    .insert("turbidity_min", turbidity_min));
-        }
+            foundation::auto_release_ptr<renderer::ShaderGroup> oslShaderGroup = renderer::ShaderGroupFactory().create("OSL_Sky");
+            oslShaderGroup->add_shader("surface", "testBG", "BGLayer", renderer::ParamArray());
+            scene->shader_groups().insert(oslShaderGroup);
+            environmentEDF = renderer::OSLEnvironmentEDFFactory().create("sky_edf", renderer::ParamArray()
+                .insert("osl_background", "OSL_Sky")
+                .insert("radiance_multiplier", environmentIntensity));
 
-        break;
-    }
-    case 6: //OSL Sky
-    {
-        foundation::auto_release_ptr<renderer::ShaderGroup> oslShaderGroup = renderer::ShaderGroupFactory().create("OSL_Sky");
-        oslShaderGroup->add_shader("surface", "testBG", "BGLayer", renderer::ParamArray());
-        scene->shader_groups().insert(oslShaderGroup);
-        environmentEDF = renderer::OSLEnvironmentEDFFactory().create("sky_edf", renderer::ParamArray()
-            .insert("osl_background", "OSL_Sky")
-            .insert("radiance_multiplier", environmentIntensity));
-
-        break;
-    }
+            break;
+        }
     };
 
     renderer::EnvironmentEDF *sky = scene->environment_edfs().get_by_name("sky_edf");
@@ -1052,6 +1051,7 @@ void AppleseedRenderer::defineLight(boost::shared_ptr<MayaObject> obj)
         renderer::ParamArray& params = light->get_parameters();
         if (isSunlight)
         {
+            defineEnvironment(); // update environment
             float turbidity = getFloatAttr("mtap_turbidity", depFn, 4.0f);
             params.insert("turbidity", turbidity);
             params.insert("radiance_multiplier", intensity);
