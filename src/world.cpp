33bed5af86b59399e5c0d9ab209337b8f4406725
est77 <ramenhdr@gmail.com>
2016-04-09 14:31:16 +0200

    Reduce timer callback frequency to keep CPU usage low.

--- src/world.cpp
+++ src/world.cpp
@@ -65,7 +65,7 @@ World::World()
 {
     // in batch mode we do not need any renderView callbacks, and timer callbacks do not work anyway in batch
     if (MGlobal::mayaState() != MGlobal::kBatch)
-        timerCallbackId = MTimerMessage::addTimerCallback(0.001, RenderQueueWorker::renderQueueWorkerTimerCallback);
+        timerCallbackId = MTimerMessage::addTimerCallback(0.1, RenderQueueWorker::renderQueueWorkerTimerCallback);
 
     std::string oslShaderPath = (getRendererHome() + "shaders").asChar();
 
